911a6da277118c75e04b5eb489cd553a6b49028e
hazeliciousness <happynijman@gmail.com>
2026-09-17 11:44:15 +0200

    Opdracht 5

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,7 +11,16 @@ namespace Idle_Game
         private DispatcherTimer autoClickTimer3;
 
         private int Strawberries = 0;
-        private int produceAmount = 1;
+        private int produceAmount = 10000;
+
+        private int upgradeAmount1 = 0;
+        private int upgradeAmount2 = 0;
+        private int upgradeAmount3 = 0;
+
+        private int upgradePrice1 = 100;
+        private int upgradePrice2 = 1000;
+        private int upgradePrice3 = 10000;
+
 
         public MainWindow()
         {
@@ -33,11 +42,6 @@ namespace Idle_Game
             autoClickTimer3 = new DispatcherTimer();
             autoClickTimer3.Interval = TimeSpan.FromMilliseconds(500);
             autoClickTimer3.Tick += AutoClickTimer_Tick;
-        }
-
-        private void AutoClickerLog(object sender, object e)
-        {
-
         }
 
         private void GameTimer_Tick(object sender, object e)
@@ -62,37 +66,82 @@ namespace Idle_Game
 
         private void Upgrade_Click1(object sender, object e)
         {
-            if (Strawberries >= 100)
+            if (Strawberries >= upgradePrice1)
             {
-                Strawberries -= 100;
+                Strawberries -= upgradePrice1;
 
-                UpgradeButton1.Visibility = Visibility.Collapsed;
+                upgradePrice1 *= 2;
+                UpgradeButton1.Content = $"Upgrade Growth Speed ({upgradePrice1})";
 
-                produceAmount = 10;
+                if (upgradeAmount1 == 0)
+                {
+                    produceAmount += 10;
+                    upgradeAmount1++;
+                }
+                else if (upgradeAmount1 == 1)
+                {
+                    produceAmount += 20;
+                    upgradeAmount1++;
+                }
+                else if (upgradeAmount1 == 2)
+                {
+                    produceAmount += 40;
+                    UpgradeButton1.Visibility = Visibility.Collapsed;
+                }
             }
         }
 
         private void Upgrade_Click2(object sender, object e)
         {
-            if (Strawberries >= 1000)
+            if (Strawberries >= upgradePrice2)
             {
-                Strawberries -= 1000;
+                Strawberries -= upgradePrice2;
 
-                UpgradeButton2.Visibility = Visibility.Collapsed;
+                upgradePrice2 *= 2;
+                UpgradeButton2.Content = $"Upgrade Watering System ({upgradePrice2})";
 
-                produceAmount = 50;
+                if (upgradeAmount2 == 0)
+                {
+                    produceAmount += 50;
+                    upgradeAmount2++;
+                }
+                else if (upgradeAmount2 == 1)
+                {
+                    produceAmount += 100;
+                    upgradeAmount2++;
+                }
+                else if (upgradeAmount2 == 2)
+                {
+                    produceAmount += 200;
+                    UpgradeButton2.Visibility = Visibility.Collapsed;
+                }
             }
         }
 
         private void Upgrade_Click3(object sender, object e)
         {
-            if (Strawberries >= 10000)
+            if (Strawberries >= upgradePrice3)
             {
-                Strawberries -= 10000;
+                Strawberries -= upgradePrice3;
 
-                UpgradeButton3.Visibility = Visibility.Collapsed;
+                upgradePrice3 *= 2;
+                UpgradeButton3.Content = $"Upgrade Fertilizer ({upgradePrice3})";
 
-                produceAmount = 100;
+                if (upgradeAmount3 == 0)
+                {
+                    produceAmount += 100;
+                    upgradeAmount3++;
+                }
+                else if (upgradeAmount3 == 1)
+                {
+                    produceAmount += 200;
+                    upgradeAmount3++;
+                }
+                else if (upgradeAmount3 == 2)
+                {
+                    produceAmount += 400;
+                    UpgradeButton3.Visibility = Visibility.Collapsed;
+                }
             }
         }
 
